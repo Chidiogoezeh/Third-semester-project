1bff37fbd206e1708071b618cb830c0727d07d64
Chidiogo <chidiogoezeh.me@gmail.com>
2026-06-04 16:26:30 +0100

    chore(events): remove redundant CreateEventInput type definitions

--- src/modules/events/event.types.ts
+++
@@ -1,9 +0,0 @@
-export type CreateEventInput = {
-  title: string;
-  description: string;
-  location: string;
-  price: number;
-  capacity?: number;
-  eventDate: string;
-  reminderWindow?: number;
-};
